33889f058bbf01a2cb7f6f2940cd72af24dc3717
maveric-ui <maveric.ui@gmail.com>
2018-11-26 15:14:09 +0200

    stable validation

--- src/components/FormEmployeeComponent/FormEmployeeComponent.js
+++ src/components/FormEmployeeComponent/FormEmployeeComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './FormEmployeeComponent.less';
 import { Button, Icon, MenuItem, Input, Select, FormControl, FormHelperText } from '@material-ui/core';
+import { validationEmployeeForm } from '../../utils/ValidationEmployeeForm';
 
 let employeeLength;
 
@@ -67,81 +68,7 @@ class FormEmployeeComponent extends Component {
   handleValueChange = name => event => {
     const value = event.target.value;
     const {errorStates, dateOfBirth} = this.state;
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth() + 1;
-    const currentDay = currentDate.getDate();
-
-    switch (name) {
-      case "name":
-        if (value.length === 0) {
-          errorStates.name = 'This field is required';
-        } else if (value.length <= 2) {
-          errorStates.name = 'This field must have more then 2 letters'
-        } else {
-          errorStates.name = null;
-        }
-        break;
-      case "dateOfBirth":
-        const getDateOfBirth = new Date(value);
-        const dateOfBirthYear = getDateOfBirth.getFullYear();
-        const dateOfBirthMonth = getDateOfBirth.getMonth() + 1;
-        const dateOfBirthDay = getDateOfBirth.getDate();
-
-        if (value.length === 0) {
-          errorStates.dateOfBirth = 'This field is required';
-        } else if (dateOfBirthYear === currentYear
-            && dateOfBirthMonth === currentMonth
-            && dateOfBirthDay === currentDay) {
-          errorStates.dateOfBirth = 'The Date of Birth can\'t be today';
-        } else if (dateOfBirthYear > currentYear
-            || dateOfBirthMonth > currentMonth
-            || dateOfBirthDay > currentDay) {
-          errorStates.dateOfBirth = 'The Date of Birth can\'t be more than current date';
-        } else {
-          errorStates.dateOfBirth = null;
-        }
-        break;
-      case "hireDate":
-        const getHireDate = new Date(value);
-        const hireDateYear = getHireDate.getFullYear();
-        const hireDateMonth = getHireDate.getMonth() + 1;
-        const hireDateDay = getHireDate.getDate();
-
-        const dateOfBirthState = new Date(dateOfBirth);
-        const dateOfBirthStateYear = dateOfBirthState.getFullYear();
-        const dateOfBirthStateMonth = dateOfBirthState.getMonth() + 1;
-        const dateOfBirthStateDay = dateOfBirthState.getDate();
-
-        if (value.length === 0) {
-          errorStates.hireDate = 'This field is required';
-        } else if (hireDateYear === dateOfBirthStateYear
-            && hireDateMonth === dateOfBirthStateMonth
-            && hireDateDay === dateOfBirthStateDay) {
-          errorStates.hireDate = 'The Hire Date  can\'t be equal to Date of Birth';
-        } else if (hireDateYear < dateOfBirthStateYear
-            || hireDateMonth < dateOfBirthStateMonth
-            || hireDateDay < dateOfBirthStateDay) {
-          errorStates.hireDate = 'The Hire Date  can\'t be less than Date of Birth';
-        } else if (hireDateYear > currentYear
-            || hireDateMonth > currentMonth
-            || hireDateDay > currentDay) {
-          errorStates.hireDate = 'The Hire Date  can\'t be more than current date';
-        } else {
-          errorStates.hireDate = null;
-        }
-        break;
-      case "country":
-        if (value.length === 0) {
-          errorStates.country = 'This field is required';
-        } else {
-          errorStates.country = null;
-        }
-        break;
-      default:
-        break;
-    }
-
+    validationEmployeeForm(name, value, errorStates, dateOfBirth);
     this.setState({errorStates, [name]: value});
   };
 
@@ -152,8 +79,20 @@ class FormEmployeeComponent extends Component {
     Object.values(errorStates).map(val => val !== null && (valid = false));
 
     if (name === "" || dateOfBirth === "" || hireDate === "" || country === "") {
+      this.setState({errorStates: {
+          name: "This field is required",
+          dateOfBirth: "This field is required",
+          hireDate: "This field is required",
+          country: "This field is required",
+        }
+      });
       valid = false;
     }
+
+    // if(name === "") {
+    //   this.setState({errorStates: {name: "This field is required"}});
+    //   valid = false;
+    // }
 
     return valid;
   };
---
+++ src/utils/ValidationEmployeeForm.js
@@ -0,0 +1,76 @@
+export const validationEmployeeForm = (name, value, errorStates, dateOfBirth) => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentDay = currentDate.getDate();
+
+  switch (name) {
+    case "name":
+      if (value.length === 0) {
+        errorStates.name = 'This field is required';
+      } else if (value.length <= 2) {
+        errorStates.name = 'This field must have more then 2 letters'
+      } else {
+        errorStates.name = null;
+      }
+      break;
+    case "dateOfBirth":
+      const getDateOfBirth = new Date(value);
+      const dateOfBirthYear = getDateOfBirth.getFullYear();
+      const dateOfBirthMonth = getDateOfBirth.getMonth() + 1;
+      const dateOfBirthDay = getDateOfBirth.getDate();
+
+      if (value.length === 0) {
+        errorStates.dateOfBirth = 'This field is required';
+      } else if (dateOfBirthYear === currentYear
+          && dateOfBirthMonth === currentMonth
+          && dateOfBirthDay === currentDay) {
+        errorStates.dateOfBirth = 'The Date of Birth can\'t be today';
+      } else if (dateOfBirthYear > currentYear
+          || dateOfBirthMonth > currentMonth
+          || dateOfBirthDay > currentDay) {
+        errorStates.dateOfBirth = 'The Date of Birth can\'t be more than current date';
+      } else {
+        errorStates.dateOfBirth = null;
+      }
+      break;
+    case "hireDate":
+      const getHireDate = new Date(value);
+      const hireDateYear = getHireDate.getFullYear();
+      const hireDateMonth = getHireDate.getMonth() + 1;
+      const hireDateDay = getHireDate.getDate();
+
+      const dateOfBirthState = new Date(dateOfBirth);
+      const dateOfBirthStateYear = dateOfBirthState.getFullYear();
+      const dateOfBirthStateMonth = dateOfBirthState.getMonth() + 1;
+      const dateOfBirthStateDay = dateOfBirthState.getDate();
+
+      if (value.length === 0) {
+        errorStates.hireDate = 'This field is required';
+      } else if (hireDateYear === dateOfBirthStateYear
+          && hireDateMonth === dateOfBirthStateMonth
+          && hireDateDay === dateOfBirthStateDay) {
+        errorStates.hireDate = 'The Hire Date  can\'t be equal to Date of Birth';
+      } else if (hireDateYear < dateOfBirthStateYear
+          || hireDateMonth < dateOfBirthStateMonth
+          || hireDateDay < dateOfBirthStateDay) {
+        errorStates.hireDate = 'The Hire Date  can\'t be less than Date of Birth';
+      } else if (hireDateYear > currentYear
+          || hireDateMonth > currentMonth
+          || hireDateDay > currentDay) {
+        errorStates.hireDate = 'The Hire Date  can\'t be more than current date';
+      } else {
+        errorStates.hireDate = null;
+      }
+      break;
+    case "country":
+      if (value.length === 0) {
+        errorStates.country = 'This field is required';
+      } else {
+        errorStates.country = null;
+      }
+      break;
+    default:
+      break;
+  }
+};
